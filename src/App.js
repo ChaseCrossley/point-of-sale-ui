af0bdf5218b8d8f7e98770e9abe18fa713130b62
dakotac5 <chasecrossley99@gmail.com>
2021-05-23 21:20:24 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import fetchUtils from "./utils/fetchUtils";
+import Grid from "./componets/grid/grid";
+import CardItem from "./componets/item/card_item/cardItem";
+
+let items = [];
+
+// fetchUtils.get("http://0.0.0.0:3724/api/item/retrieveAll").then((data) => items = data).catch((error) => {
+//     items = "An error occurred"
+//     console.log(error)
+// }).finally(() => console.log(items))
+
+items = [];
+
+for (let i = 0; i < 10; i++) {
+    let ingredients = []
+    for (let j = 0; j < 5; j++) {
+        ingredients.push("ingredient" + j)
+    }
+    items.push((<CardItem name={"item " + i} ingredients={ingredients} imageUrl={"https://source.unsplash.com/collection/75315648/1600x900"} key={i.toString()}/>))
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid numberOfColumns={3}>
+                {items}
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
