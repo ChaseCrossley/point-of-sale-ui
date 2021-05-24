13c691e746b6d8b367369ea6ff41c75fedc52950
dakotac5 <chasecrossley99@gmail.com>
2021-05-23 21:20:35 -0700

    stared addNewItem.js

---
+++ src/componets/add_new_item/addNewItem.js
@@ -0,0 +1,12 @@
+import styles from './AddNewItem.module.scss'
+
+export default function AddNewItem() {
+
+    return (
+        <div className={styles.container}>
+            <h1>
+                Add New Item
+            </h1>
+        </div>
+    )
+}
