66c59ffa3fea1a7e2f4bf728704818297d07e028
dakotac5 <chasecrossley99@gmail.com>
2021-05-23 21:19:09 -0700

    created grid.js and css for component

---
+++ src/componets/grid/grid.js
@@ -0,0 +1,23 @@
+import styles from './Grid.module.scss'
+
+export default function Grid(props) {
+
+    let rows = [];
+    let row = [];
+    for (let i = 0; i < props.children.length; i++) {
+        row.push(props.children[i])
+        if (row.length === props.numberOfColumns) {
+            rows.push((<div key={rows.length+1} className={styles.row}>{row}</div>));
+            row = []
+        }
+    }
+    if (row.length > 0) {
+        rows.push((<div key={rows.length+1} className={styles.row}>{row}</div>));
+    }
+
+    return (
+        <div className={styles.container}>
+            {rows}
+        </div>
+    )
+}
