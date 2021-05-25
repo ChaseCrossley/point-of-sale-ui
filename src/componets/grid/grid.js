eb124c5c491be21dcd3574f93cb629e9a497fb0c
dakotac5 <chasecrossley99@gmail.com>
2021-05-24 22:05:25 -0700

    updated cardItem.js and grid.js

--- src/componets/grid/grid.js
+++ src/componets/grid/grid.js
@@ -7,12 +7,12 @@ export default function Grid(props) {
     for (let i = 0; i < props.children.length; i++) {
         row.push(props.children[i])
         if (row.length === props.numberOfColumns) {
-            rows.push((<div key={rows.length+1} className={styles.row}>{row}</div>));
+            rows.push((<div key={rows.length + 1} className={styles.row}>{row}</div>));
             row = []
         }
     }
     if (row.length > 0) {
-        rows.push((<div key={rows.length+1} className={styles.row}>{row}</div>));
+        rows.push((<div key={rows.length + 1} className={styles.row}>{row}</div>));
     }
 
     return (
--- src/componets/item/card_item/cardItem.js
+++ src/componets/item/card_item/cardItem.js
@@ -1,51 +1,40 @@
 import styles from './CardItem.module.scss'
 import {Component} from "react";
 import loading_gif from "../../../assets/loading.gif"
-import {createApi} from 'unsplash-js';
-
-// on your node server
-const serverApi = createApi({
-    accessKey: '6NmQ3bI0CYjPkEa5R6-HSvgRkXWpbglcJhG-oY8W2fc',
-});
-
 
 export default class CardItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: props.name,
-            ingredients: props.ingredients,
-            imagePath: loading_gif
+            item: props.item,
+            imagePath: props.imagePath
+
         }
-        this.sleep(2000, () => {
-            console.log(this.id)
-            this.setState({imagePath: "https://source.unsplash.com/collection/75315648/1600x900?sig=" + this.getRandomInt(0, 10000000)});
-            this.id += 1
-            console.log(this.id)
-
-        })
-    }
-
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        this.handleClick = props.onClick
     }
 
 
-    async sleep(ms, callback) {
-        await new Promise(resolve => setTimeout(resolve, ms));
-        callback()
-    }
 
     render() {
-        let ingredients = this.state.ingredients.join(", ")
+        if (!this.state.item) {
+            return (
+                <div className={styles.container}>
+                    <img className={styles.image} src={loading_gif} alt={"loading"}/>
+                </div>
+            )
+        }
+
+        const item = this.state.item;
+        let ingredients = []
+        item.ingredientItems.forEach((item) => ingredients.push(item.ingredient.name))
+        ingredients = ingredients.join(", ")
+
         return (
-            <div className={styles.container}>
-                <img className={styles.image} src={this.state.imagePath} alt={this.state.name}/>
+            <div className={styles.container} onClick={this.handleClick}>
+                <img className={styles.image} src={this.state.imagePath} alt={item.name}/>
                 <div className={styles.text}>
                     <h3 className={styles.name}>
-                        {this.state.name}
+                        {item.name}
                     </h3>
                     <p className={styles.ingredient}>
                         {ingredients}
